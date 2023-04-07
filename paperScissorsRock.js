4b8f0671ccf29afce79c2760b7ac2c5e30679f72
dwcarey <danielwilliamcarey@gmail.com>
2023-04-07 23:10:34 +0800

    complete and working holy moly

--- paperScissorsRock.js
+++ paperScissorsRock.js
@@ -3,144 +3,75 @@ let playerChoice;
 let playerScore = 0;
 let computerScore = 0;
 
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const rock = document.getElementById('rock');
 
-//this function defines an array of 3 choices
-//then assigns as a variable a random number based on the array length (0-2)
-//then returns an array object based on the random number
 
-function getComputerChoice(computerChoice) {
+paper.addEventListener('click', function() {
+  playRound('Paper');
+});
+
+scissors.addEventListener('click', function() {
+  playRound('Scissors');
+});
+
+rock.addEventListener('click', function() {
+  playRound('Rock');
+});
+
+
+function getComputerChoice() {
     const arrayChoices = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor(Math.random() * arrayChoices.length);
     return arrayChoices[randomNumber];
 }
 
-//this function prompts the user for text input "rock paper or scissors"
-//then slices it (first letter and 2nd letter to end)
-//then converts first letter to upper case and all others to lower case for easy use in gameplay function
-
-function getPlayerChoice(playerChoice){
-    let playerChoicePrompt = prompt("Paper, Scissors or Rock?", "One of those")
-    let firstLetter = playerChoicePrompt.slice(0, 1);
-    let theRest = playerChoicePrompt.slice(1);
-    let firstLetterCap = firstLetter.toUpperCase();
-    let theRestUnCap = theRest.toLowerCase();
-    return firstLetterCap+theRestUnCap;
-}
-//get choices, evaluates if player win or lose , returns string player wins, player loses, draw
-//increment player or computer score by 1 per round
-
-function playRound() {
-        computerChoice = getComputerChoice();
-        playerChoice = getPlayerChoice();
-        
-        if (playerChoice == computerChoice)
-        {return 'It\'s a draw!';
-        }
+function playRound(playerChoice) {
+  computerChoice = getComputerChoice();
   
-        else if (playerChoice == 'Rock' && computerChoice == 'Scissors' 
-        || playerChoice == 'Paper' && computerChoice == 'Rock'
-        || playerChoice == 'Scissors' && computerChoice == 'Paper')
-        {playerScore++;
-        return 'Player Wins!';
-        }
-
-        else if 
-        (computerChoice == 'Rock' && playerChoice == 'Scissors' 
-        || computerChoice == 'Paper' && playerChoice == 'Rock'
-        || computerChoice == 'Scissors' && playerChoice == 'Paper')
-        {computerScore++;
-        return 'Computer Wins!';
-        }
-
-        else return 'What did you do???????';
-
-}
-
-function playGame() {
-  while (playerScore < 3 && computerScore < 3) {
-  let result = playRound();
-  alert(`${result} Player Choice: ${playerChoice}, Computer Choice: ${computerChoice}`);
-  alert(`Player Score: ${playerScore}, Computer Score: ${computerScore}.`);
+  if (playerChoice == computerChoice) {
+    document.querySelector('.winnerText').textContent = 'It\'s a draw!';
+  } else if (playerChoice == 'Rock' && computerChoice == 'Scissors' || 
+             playerChoice == 'Paper' && computerChoice == 'Rock' || 
+             playerChoice == 'Scissors' && computerChoice == 'Paper') {
+    playerScore++;
+    document.querySelector('.winnerText').textContent = 'You Win!';
+  } else if (computerChoice == 'Rock' && playerChoice == 'Scissors' || 
+             computerChoice == 'Paper' && playerChoice == 'Rock' || 
+             computerChoice == 'Scissors' && playerChoice == 'Paper') {
+    computerScore++;
+    document.querySelector('.winnerText').textContent = 'Computer Wins!';
   }
-  if (playerScore == 3) {alert(`Player Wins the game! Player Score: ${playerScore}.`);
-  playerScore = 0;
-  computerScore = 0;
-  playGame();
+
+  document.querySelector('.theChoices').textContent = `You Chose: ${playerChoice}, \nComputer Chose: ${computerChoice}`;
+  document.querySelector('.gameScore').textContent = `Your Score: ${playerScore}. Computer Score: ${computerScore}.`;
+  setTimeout(restart, 100);
+
+
 }
 
-  else if (computerScore == 3) {alert(`Computer Wins the game! Computer Score: ${computerScore}.`);
-  playerScore = 0;
-  computerScore = 0;
-  playGame();
+  
+
+function restart() {  
+  if (playerScore === 5) {
+    alert('Game Over! \nYou Win!!!!!!');
+    playerScore = 0;
+    computerScore = 0;
+    document.querySelector('.winnerText').textContent = 'Playing again?';
+    document.querySelector('.theChoices').textContent = 'I really don\'t see why..';
+    document.querySelector('.gameScore').textContent = 'Oh well, have fun!';
+
+  }
+  else if (computerScore === 5) {
+    alert('Game Over! \nYou Lose!!!!!!');
+    playerScore = 0;
+    computerScore = 0;
+    document.querySelector('.winnerText').textContent = 'Playing again?';
+    document.querySelector('.theChoices').textContent = 'I really don\'t see why..';
+    document.querySelector('.gameScore').textContent = 'Oh well, have fun!';
+  }
+
+
 }
-}
-
-
-
-
-
-
-
-console.log(playGame());
-console.log(playRound());
-console.log(playerChoice);
-console.log(computerChoice);
-console.log(playerScore);
-console.log(computerScore);
-
-
-
-
-//this returns the desired text for later use as paragraph output
-//based on winner/loser of the round
-//use this function INSIDE main game function
-
-//function playRoundTextOutput(computerChoice, playerChoice) {
-  //  if (computerChoice == playerChoice) 
-  //  return `${playerChoice} is the same as ${computerChoice}, amazing!`;
-
- //   else if (playerChoice == "Rock" && computerChoice == "Paper") 
- //   return `${playerChoice} sucks, ${computerChoice} fucks, you lose!`;
-
- //   else if (playerChoice == "Rock" && computerChoice == "Scissors") 
- //   return `${playerChoice} is dank, ${computerChoice} stank, you win!`;
-
- //   else if (playerChoice == "Paper" && computerChoice == "Scissors") 
-  //  return `${playerChoice} is nasty, ${computerChoice} are fasty, you lose!`;
-
-  //  else if (playerChoice == "Paper" && computerChoice == "Rock") 
-    //return `${playerChoice} is l33t, ${computerChoice} can\'t comp3t3, you win!`; 
-
- //   else if (playerChoice == "Scissors" && computerChoice == "Paper") 
- //   return `${playerChoice} has problems, ${computerChoice} ain\'t one of them, you win!`;
-
- //   else if (playerChoice == "Scissors" && computerChoice == "Rock") 
- //   return `${playerChoice} don/'t rhyme, ${computerChoice} ain\'t got the time, you lose!`;
-
- //   else return `${playerChoice} aint shit`;
-
-//}
-//this is supposed to be increments playerScore or computerScore by 1 on victory
-//i think  it all needs to go inside game function
-
-//function gameScore(playerChoice, computerChoice) {
- //   if (playerChoice == "Rock" && computerChoice == "Paper" 
- //   || playerChoice == "Paper" && computerChoice == "Scissors"
-  //  || playerChoice == "Scissors" && computerChoice == "Rock")
-  //  return ++computerScore;
-
- //   else if (computerChoice == "Rock" && playerChoice == "Paper" 
- //   || computerChoice == "Paper" && playerChoice == "Scissors"
- //   || computerChoice == "Scissors" && playerChoice == "Rock")
- //   return ++playerScore;
-
- //   else return "oh dear";
-
-//}
-
-//function playGame() {}
-
-
-
-
 
